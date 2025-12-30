8906b8be5204916902e6a7226411b877c3fb6283
matin <matin.akbari06ap@gmail.com>
2025-12-30 17:17:07 +0330

    Create Users class

---
+++ LocalUser.h
@@ -0,0 +1,14 @@
+//
+// Created by matin on 12/30/25.
+//
+
+#ifndef MESSENGER_LOCALUSER_H
+#define MESSENGER_LOCALUSER_H
+
+#include "User.h"
+
+class LocalUser : public User{
+};
+
+
+#endif //MESSENGER_LOCALUSER_H
---
+++ User.h
@@ -0,0 +1,25 @@
+//
+// Created by matin on 12/30/25.
+//
+
+#ifndef MESSENGER_USER_H
+#define MESSENGER_USER_H
+
+#include <string>
+
+
+using namespace std;
+
+class User {
+protected:
+    int userId = 0; // check
+    string username;
+    string fullName;
+    string password;
+    //Date , birthdate , joindate
+public:
+
+};
+
+
+#endif //MESSENGER_USER_H
