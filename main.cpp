92df2e28dca65729c97599236178bcc9a428c284
matin <matin.akbari06ap@gmail.com>
2025-12-30 17:09:44 +0330

    create main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "main.h"
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,19 @@
+//
+// Created by matin on 12/30/25.
+//
+
+#ifndef MESSENGER_MAIN_H
+#define MESSENGER_MAIN_H
+
+#include <iostream>
+
+#include "ConsoleStyle.h"
+
+
+
+
+
+
+int main();
+
+#endif //MESSENGER_MAIN_H
